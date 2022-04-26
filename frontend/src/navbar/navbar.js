75f1b51eee4e47eb9755f5439d9a7a20e92ece7c
Rich <richrar@gmail.com>
2022-04-26 12:15:15 +0100

    navbar basic structure

---
+++ frontend/src/navbar/navbar.js
@@ -0,0 +1,17 @@
+
+
+const Navbar = (props) => {
+
+    const
+
+    return (
+        <div>
+            <img src={props.avatar} alt={props.username + "avatar"} />
+            <img src="./lemon.png" alt="a picture of a damn lemon" />
+            <svg onClick={handleClick}> path </svg>
+        </div>
+
+    );
+}
+
+export default Navbar
