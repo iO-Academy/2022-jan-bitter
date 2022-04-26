3826d09d6e1de5a60de151a1012f9a91d1c4d435
Charlotte Buckley <cfbuckley14@gmail.com>
2022-04-26 14:22:39 +0100

    setting up ProfilePage component

---
+++ frontend/src/Templates/ProfilePage/ProfilePage.js
@@ -0,0 +1,19 @@
+const ProfilePage = () => {
+    return (
+        <div>
+            <Navbar />
+            <header>
+                <ProfileImage />
+                <ProfileAbout />
+            </header>
+            <main>
+                <h2>Bleats</h2>
+                <div>
+                    <p>Your bleats go here!</p>
+                </div>
+            </main>
+        </div>
+    )
+}
+
+export default ProfilePage
