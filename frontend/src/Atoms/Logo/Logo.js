6361780d06866690219a0ced41e55e34c147385d
Rich <richrar@gmail.com>
2022-04-26 14:37:27 +0100

    Bittr logo component and image file from canva

---
+++ frontend/src/Atoms/Logo/Logo.js
@@ -0,0 +1,10 @@
+
+const BittrLogo = (props) => {
+    return (
+        <>
+        <img className={props.style} src="Bittr-logo.png" alt="Bitter lemon logo for Bittr" />
+        </>
+    )
+}
+
+export default BittrLogo
