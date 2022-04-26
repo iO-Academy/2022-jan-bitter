a98996128c0fd5b76c9982ea8c885376979fa6d1
Rich <richrar@gmail.com>
2022-04-26 16:38:41 +0100

    files moved

---
+++ frontend/src/Organisms/Navbar/Navbar.js
@@ -0,0 +1,14 @@
+import BittrLogo from "../../Atoms/BittrLogo/BittrLogo";
+
+const Navbar = (props) => {
+
+    return (
+        <nav>
+            <ProfileImage />
+            <BittrLogo />
+            <Button />
+        </nav>
+    )
+}
+
+export default Navbar
