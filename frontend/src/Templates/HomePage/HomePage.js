d5ca19b26d2a8a1417468853fcdefb86de4a459d
oritchie77 <96186469+oritchie77@users.noreply.github.com>
2022-04-28 16:42:17 +0100

    basic structure created

---
+++ frontend/src/Templates/HomePage/HomePage.js
@@ -0,0 +1,16 @@
+const HomePage = () => {
+    return(
+        <>
+            <main className={'home-page'}>
+                <div className={'write-bleat-container'}>
+                    {/*insert componet here */}
+                </div>
+                <div className={'bleats-feed-container'}>
+                    {/*insert componet here */}
+                </div>
+            </main>
+        </>
+    )
+}
+
+export default HomePage
