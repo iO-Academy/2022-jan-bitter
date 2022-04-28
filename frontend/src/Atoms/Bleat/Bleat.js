9068fe639a45bfc05d67e2eafd0d48bd451a3718
Rich <richrar@gmail.com>
2022-04-28 14:00:30 +0100

    Bleat component

--- frontend/src/Atoms/Bleat/Bleat.js
+++ frontend/src/Atoms/Bleat/Bleat.js
@@ -1,15 +1,16 @@
-import ProfileImage from "../ProfileImage/ProfileImage";
-
+import './Bleat.scss'
 
 const Bleat = (props) => {
     return (
         <div className="bleatCard">
+            <img className="profile-icon-placeholder" src="/images/profile-icon-placeholder.png" alt="A placeholder image for the user profile" />
             <div className="bleatContents">
-                <ProfileImage />
-                <h2>{props.username}</h2>
-                <p>{props.bleat}</p>
-                bleat time
+                <h2>@{props.username}</h2>
+                <p className="bleatText">{props.bleat}</p>
+                <p className="bleatTime"> bleated: {props.bleatTime}</p>
             </div>
         </div>
     )
 }
+
+export default Bleat
